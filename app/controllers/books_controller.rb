f4ef8cfa53eebc8688f7e061b0ee41f03db51c2a
areebaayyaz <areebaayyaz@gmail.com>
2019-07-05 05:59:24 -0700

    commit after CRUD operations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,62 @@
 class BooksController < ApplicationController
-
+include BooksHelper
+ 
  def index
  
   @books = Book.all
 
  end
 
+ 
  def show
 
   @book = Book.find(params[:id])
 
  end
+
+ 
+ def new
+
+  @book = Book.new
+
+ end
+
+ 
+ def create
+
+ @book = Book.new(book_params)
+ @book.save
+
+ redirect_to book_path(@book)
+
+ end
+
+
+ def destroy
+
+  @book = Book.destroy(params[:id])
+  redirect_to "/books"
+  flash.notice = "Book: '#{@book.title}' Deleted!"
+
+ end
+
+ 
+ def edit
+  
+  @book = Book.find(params[:id])
+
+ end
+
+ 
+ def update
+  
+  @book = Book.find(params[:id])
+  @book.update(book_params)
+
+  redirect_to book_path(@book)
+  flash.notice = "Book: '#{@book.title}' Updated!"
+
+ end
+ 
 
 end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,7 @@
 module BooksHelper
+
+def book_params
+ params.require(:book).permit(:title, :author, :no_of_copies)
+end
+
 end
